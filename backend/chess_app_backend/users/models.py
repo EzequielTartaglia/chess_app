43f74e34329632c3e1fbda52683ab2113ee3b930
EzequielTartaglia <ezequielmtartaglia@gmail.com>
2025-05-23 01:48:52 -0300

    feat: Add tournaments and users apps with models, migrations, and admin configurations

---
+++ backend/chess_app_backend/users/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.username
+    def user_details(self, id):
+        user = User.objects.get(id=id)
+        return {
+            'username': user.username,
+            'email': user.email,
+            'first_name': user.first_name,
+            'last_name': user.last_name,
+        }
