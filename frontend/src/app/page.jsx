931e92a21e38e431617b9fca8a0088950a68c85f
EzequielTartaglia <ezequielmtartaglia@gmail.com>
2025-05-23 14:08:23 -0300

    feat: actualizar colores de fondo y texto en HomePage para usar variables de Tailwind CSS

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,12 +1,11 @@
 import Button from "@/components/Button";
 
 export default function HomePage() {
-
   return (
-    <div className="relative w-[1920px] h-[1080px] bg-[#101010] overflow-hidden">
+    <div className="relative w-[1920px] h-[1080px] bg-primary overflow-hidden">
       {/* Rombos decorativos */}
       <div className="absolute w-[621.82px] h-[622.53px] left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 bg-[#F5E2A0] rotate-[135deg]" />
-      <div className="absolute w-[194px] h-[194px] left-[860.18px] top-[15%] bg-[#101010] rounded-[38px] rotate-45" />
+      <div className="absolute w-[194px] h-[194px] left-[860.18px] top-[15%] bg-primary rounded-[38px] rotate-45" />
 
       {/* Título */}
       <div
@@ -16,7 +15,7 @@ export default function HomePage() {
           w-[268px] h-[114px] 
           font-['Agency_FB'] font-bold 
           text-[61px] leading-[50px] 
-          text-[#101010]
+          text-secondary
           flex items-center justify-center
           "
       >
@@ -32,7 +31,7 @@ export default function HomePage() {
           font-['Agency_FB'] font-bold 
           text-[10px] leading-[12px] 
           text-justify 
-          text-[#101010]"
+         text-secondary"
       >
         A new way to play the consecrated game of chess. A decentralized
         platform for everyone who wants to earn money playing the most beloved
