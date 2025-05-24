1088afab1abbb598a3e6288f942d7f88c5e75a73
EzequielTartaglia <ezequielmtartaglia@gmail.com>
2025-05-23 22:07:27 -0300

    feat: importar componente CompanyLogo en RegisterPage

--- frontend/src/Views/Register/RegisterPage.jsx
+++ frontend/src/Views/Register/RegisterPage.jsx
@@ -4,6 +4,7 @@ import { useRouter } from "next/navigation";
 
 import Link from "next/link";
 import Image from "next/image";
+import CompanyLogo from "@/components/CompanyLogo";
 
 export default function RegisterPage() {
   const [fname, setFname] = useState("");
